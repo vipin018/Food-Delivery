bac519b4a1d3afa15b60c1caf723c0b2fc53e1dc
Vipin Pathak <126965441+vipin018@users.noreply.github.com>
2024-08-18 12:24:55 +0530

    Update StoreContext.jsx
    updated the url

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -7,7 +7,7 @@ const StoreContextProvider = (props) => {
 
     const [cartItems, setCartItems] = useState({});
 
-    const url = "http://localhost:4000"
+    const url = "https://food-delivery-backend-slve.onrender.com"
 
     const [token, setToken] = useState("");
 
